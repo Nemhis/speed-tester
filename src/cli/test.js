953b702d9140af37f145e0e62218d55a8887eca1
Nemhis <p.nemphis@gmail.com>
2020-04-02 19:39:28 +0300

    Add headless option

--- src/cli/test.js
+++ src/cli/test.js
@@ -2,17 +2,18 @@
 
 const fs = require(`fs`);
 const program = require(`commander`);
-const puppeteer = require('puppeteer');
-const {mSecondToSeconds} = require('../utility');
+const puppeteer = require(`puppeteer`);
+const {mSecondToSeconds} = require(`../utility`);
 
 const DEFAULT_COUNT = 1;
-const FILE_NAME = 'test-result.json';
+const FILE_NAME = `test-result.json`;
 
 const toUrl = (string) => new URL(string);
 
 program
   .requiredOption(`--url <url>`, `Page url for test`, toUrl)
-  .option(`--count <count>`, `Count test iteration`, 1);
+  .option(`--count <count>`, `Count test iteration`, 1)
+  .option(`--headfull`, `Count test iteration`, false);
 
 const processMetrics = (metrics) => ({
   dnsLookup: metrics.domainLookupEnd - metrics.domainLookupStart,
@@ -31,7 +32,7 @@ const metricsToSeconds = (metrics) => {
   Object
     .keys(metrics)
     .forEach((metricName) => {
-      optimized[metricName] = mSecondToSeconds(metrics[metricName])
+      optimized[metricName] = mSecondToSeconds(metrics[metricName]);
     });
 
   return optimized;
@@ -49,7 +50,7 @@ const metricsAvg = (metrics) => {
         }
 
         metricsSum[metricName] += metric[metricName];
-      })
+      });
   });
 
   Object
@@ -61,12 +62,12 @@ const metricsAvg = (metrics) => {
   return metricsSum;
 };
 
-async function test(url, count) {
-  const browser = await puppeteer.launch();
+async function test(url, count, pupeteerConfig) {
+  const browser = await puppeteer.launch(pupeteerConfig);
   const metrics = [];
 
-  for (var i = 0; i < count; i++) {
-    const page = await browser.newPage();
+  for (let i = 0; i < count; i++) {
+    const page = await browser.newPage(pupeteerConfig);
     await page.goto(url);
 
     const metricsJson = await page.evaluate(() => JSON.stringify(window.performance.timing));
@@ -85,17 +86,21 @@ module.exports = {
   name: `--test`,
   run(args) {
     program.parse(args);
-    let {url, count} = program.opts();
-    count = parseInt(count) || DEFAULT_COUNT;
+    let {url, count, headfull} = program.opts();
+    count = parseInt(count, 10) || DEFAULT_COUNT;
+    const pupeteerConfig = {
+      headless: !headfull
+    };
 
-    test(url, count)
+    test(url, count, pupeteerConfig)
       .then((metrics) => {
         let avgMetrics = metricsAvg(metrics);
         avgMetrics = metricsToSeconds(avgMetrics);
         const result = {
           url,
           count,
-          avgMetrics
+          avgMetrics,
+          metrics,
         };
 
         fs.writeFile(FILE_NAME, JSON.stringify(result), (err) => {
@@ -105,6 +110,6 @@ module.exports = {
 
           return console.info(`Operation success. File created.`);
         });
-      })
+      });
   }
 };
